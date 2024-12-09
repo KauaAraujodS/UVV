da93823bd16acc6d7b211d6fa9e6fb9d016c3188
Kauaaraujo <122256165+KauaAraujodS@users.noreply.github.com>
2024-12-09 16:42:59 -0300

    Add files via upload

---
+++ c/ABRANTES C/imc-handout/imc-handout/imc.c
@@ -0,0 +1,78 @@
+/**
+ * IDENTIFICAÇÃO DO ESTUDANTE:
+ * Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+ * nenhuma linha deste comentário de seu código!
+ *
+ *    Nome completo:
+ *    Matrícula:
+ *    Turma:
+ *    Email:
+ *
+ * DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+ * Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+ * pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+ * e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+ * Estou ciente de que todo código enviado será verificado automaticamente
+ * contra plágio e que caso eu tenha praticado qualquer atividade proibida
+ * conforme as normas da disciplina, estou sujeito à penalidades conforme
+ * definidas pelo professor da disciplina e/ou instituição.
+ */
+
+
+/**
+ * Inclusão de bibliotecas:
+ */
+#include <cs50.h>
+#include <stdio.h>
+
+
+/**
+ * Função main
+ */
+int main(void)
+{
+    // TODO: escreva aqui o código que solicita o peso da pessoa (em kg).
+    // Seu código só deve aceitar valores válidos entre 40,0 e 200,0 kg
+    // (inclusive). Se o peso informado não estiver nessa faixa ou se o usuário
+    // informar um dado inválido, seu programa deve continuar a solicitar o peso
+    // até que ele informe um peso válido.
+    float imc, peso, altura;
+    printf("Informe seu peso: ");
+    scanf("%f", &peso);
+
+    // TODO: escreva aqui o código que solicita a altura da pessoa (em m)
+    // Seu código só deve aceitar valores válidos entre 1,40 e 2,50 m
+    // (inclusive). Se a altura informada não estiver nessa faixa ou se o
+    // usuário informar um dado inválido, seu programa deve continuar a
+    // solicitar a altura até que ele informe uma altura válida.
+    printf("Informe sua altura: ");
+    scanf("%f", altura);
+    
+    // TODO: escreva aqui o código que calcula o IMC e imprime a classificação
+    // da obesidade no terminal, conforme as especificações do exercício.
+    printf("IMC: %.2f; Classificação: ", imc);
+    imc = peso/(altura*altura);
+    if (imc < 16.0) {
+        printf("Magreza grau III.\n");
+    } else if (imc < 17.0) {
+        printf("Magreza grau II.\n");
+    } else if (imc < 18.5) {
+        printf("Magreza grau I.\n");
+    } else if (imc < 25.0) {
+        printf("Peso adequado.\n");
+    } else if (imc < 30.0) {
+        printf("Pré-obeso.\n");
+    } else if (imc < 35.0) {
+        printf("Obesidade grau I.\n");
+    } else if (imc < 40.0) {
+        printf("Obesidade grau II.\n");
+    } else {
+        printf("Obesidade grau III.\n");
+    }
+    
+
+    
+    // Termina o programa:
+    return 0;
+}
+
---
+++ c/ABRANTES C/lampadas-handout/lampadas.c
@@ -0,0 +1,76 @@
+/* IDENTIFICAÇÃO DO ESTUDANTE:
+ * Preencha seus dados e leia a declaração de honestidade abaixo. NÃO APAGUE
+ * nenhuma linha deste comentário de seu código!
+ *
+ *    Nome completo:
+ *    Matrícula:
+ *    Turma:
+ *    Email:
+ *
+ * DECLARAÇÃO DE HONESTIDADE ACADÊMICA:
+ * Eu afirmo que o código abaixo foi de minha autoria. Também afirmo que não
+ * pratiquei nenhuma forma de "cola" ou "plágio" na elaboração do programa,
+ * e que não violei nenhuma das normas de integridade acadêmica da disciplina.
+ * Estou ciente de que todo código enviado será verificado automaticamente
+ * contra plágio e que caso eu tenha praticado qualquer atividade proibida
+ * conforme as normas da disciplina, estou sujeito à penalidades conforme
+ * definidas pelo professor da disciplina e/ou instituição.
+ */
+
+// Comece aqui seu programa
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * Variáveis e contantes globais
+ */
+const int BITS_NO_BYTE = 8;
+
+
+/**
+ * Protótipos das funções
+ */
+void imprimir_lampada(int bit);
+
+
+/**
+ * MAIN
+ */
+int main(void)
+{
+    // TODO: escreva aqui o seu programa!
+    
+    // Termina o programa
+    return 0;
+}
+
+
+/**
+ * IMPRIMIR_LAMPADA
+ * Esta função recebe um inteiro que representa um bit (0 ou 1) e imprime no
+ * terminal um Emoji que representa uma lâmpada acesa ou uma lâmpada apagada.
+ * Não há nenhum retorno, só a impressão de um lâmpada acessa ou apagada.
+ * Note como um Emoji é impresso através de seu código Unicode.
+ *
+ * Parâmetros:
+ *    (int) bit: um número inteiro que pode ser 0 ou 1
+ *
+ * Retorno:
+ *    (void)
+ */
+void imprimir_lampada(int bit)
+{
+    if (bit == 0)
+    {
+        // Emoji que representa uma lâmpada apagada
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Emoji que representa uma lâmpada acessa
+        printf("\U0001F7E1");
+    }
+}
+
